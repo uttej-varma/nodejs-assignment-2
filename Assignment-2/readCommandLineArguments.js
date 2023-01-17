6e3f20d549c6fb4913b35f38f827bd78ec24d37e
uttej varma <uttej2233@gmail.com>
2023-01-17 13:39:13 +0530

    assignment-2

---
+++ Assignment-2/readCommandLineArguments.js
@@ -0,0 +1,2 @@
+
+console.log("hello"+" "+process.argv[2]);
---
+++ Assignment-2/readEnvVariable.js
@@ -0,0 +1,2 @@
+console.log("Hello"+" "+process.env.USERNAME);
+//used cmd to add new value in process.env object=>   $env:USERNAME:"Uttej"
